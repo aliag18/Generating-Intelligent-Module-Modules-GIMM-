c7bd1ebd0fff347b7060642691e677ba69529813
aliag18 <aliagola@icloud.com>
2025-02-20 15:55:23 -0800

    Adjustments to run model with different data
    data.py
    - Options for which data to run on
    - commented sections unused by Nimba sites
    
    test.py
    - general tidying/commenting
    - removed longwave radiation
    - specify column header pattern for load data
    - transform NaN data
    - remove redundant plot

--- data.py
+++ data.py
@@ -1,18 +1,23 @@
 import pandas as pd
 import os
 
-# select_stations = [121,41,53,96,46,97,33,123]
-select_stations = [121]
+# Site used
+file_name = 'SD78_historic_data.csv'
+# file_name = '2011-2021_reduced.csv'
+# file_name = 'test_file.csv'
 
-load_data = pd.read_csv(os.path.join(os.path.dirname(__file__), '2011-2021_reduced.csv'))
+# select_stations = [121,41,53,96,46,97,33,123]
+select_stations = [1]
+
+load_data = pd.read_csv(os.path.join(os.path.dirname(__file__), file_name))
 load_data['time'] = pd.to_datetime(load_data.time)
 
-dropped = []
-for i in select_stations:
-    n = str(i)
-    load_data['windm'+n] = (load_data['windz'+n]**2 + load_data['windm'+n]**2)**0.5
-    load_data = load_data.rename(columns={'windm'+n:'wind'+n})
-    dropped += ['windz'+n]
+# dropped = []
+# for i in select_stations:
+#     n = str(i)
+#     load_data['windm'+n] = (load_data['windz'+n]**2 + load_data['windm'+n]**2)**0.5
+#     load_data = load_data.rename(columns={'windm'+n:'wind'+n})
+#     dropped += ['windz'+n]
 
     
 load_data['day'] = pd.DatetimeIndex(load_data['time']).day_of_week
@@ -20,4 +25,4 @@ load_data['hour'] = pd.DatetimeIndex(load_data['time']).hour
 load_data['month'] = pd.DatetimeIndex(load_data['time']).month
 
 load_data = load_data.set_index(['time'])
-load_data = load_data.drop(dropped, axis=1)
+# load_data = load_data.drop(dropped, axis=1)
--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from sklearn.metrics import r2_score
 
 import tensorflow
 from tensorflow import keras
-from keras.models import Sequential
+from keras.models import Sequential, save_model, load_model
 from keras.layers import LSTM, Dense, Dropout,  Flatten
 
 import matplotlib.pyplot as plt
@@ -21,9 +21,13 @@ import numpy as np
 import datetime
 import csv
 
-# how to run model vs how to use model as a callable function
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.rcParams.update({'font.size': 16})
 
 
+# Set up time resolution and steps
 start = datetime.datetime.now()
 
 past_hours = 6
@@ -31,40 +35,29 @@ n_steps = past_hours
 next_hours = 4
 next_steps = next_hours
 
-transformations = [e for n in select_stations for e in ('tmp'+str(n), 'swrad'+str(n), 'lwrad'+str(n), 'wind'+str(n))]
-print(transformations)
+# Configure Dataset Transforms
+transformations = [e for n in select_stations for e in ('tmp'+str(n), 'swrad'+str(n), 'wind'+str(n))]
 
 transformations = [(str(i), MinMaxScaler(copy=True), make_column_selector(pattern = e)) for i,e in enumerate(transformations)]
-# <sklearn.compose._column_transformer.make_column_selector object at 0x7fef781b29b0>
-# for i,e in enumerate(transformations):
-#     print(str(i))
-#     print(e)
-# print(transformations)
 
 transformations += [(str(len(transformations)),OneHotEncoder(), make_column_selector(pattern = 'hour'))]
 
-
 transformations += [(str(len(transformations)),OneHotEncoder(), make_column_selector(pattern = 'day'))]
-
 
 transformations += [(str(len(transformations)),OneHotEncoder(), make_column_selector(pattern = 'month'))]
 
-
-transformations += [("y",StandardScaler(copy=True), make_column_selector(pattern = 'ERCOT'))]
-
+transformations += [("y",StandardScaler(copy=True, with_mean=False), make_column_selector(pattern = 'load'))]
 
 ct = ColumnTransformer(transformations)
-# print(ct)
 
-# normed_data = pd.DataFrame(ct.fit_transform(load_data)).astype(np.float32)
 # Transform the data with the ColumnTransformer
 normed_data = ct.fit_transform(load_data)
 
 # Convert the sparse matrix to a dense array and then to a DataFrame
 normed_data = pd.DataFrame(normed_data.toarray()).astype(np.float32)
 
-dataset = normed_data.values
-print(dataset)
+# Replace any NaN data with mean value (small values result from data normalization when data is close to the mean value)
+normed_data = normed_data.fillna(normed_data.mean()) 
 
 dataset = normed_data.values
 steps = n_steps+next_steps
@@ -74,8 +67,6 @@ print(time_series.shape)
 
 X = time_series[:, :n_steps, :]
 Y = time_series[:, n_steps:, -1]
-
-print(X.shape, Y.shape)
 
 #sizes for test, train, and validation datasets
 a = len(time_series)
@@ -89,6 +80,10 @@ X_test,     Y_test  =   X[b+c:],    Y[b+c:]
 print(X_train.shape, X_val.shape, X_test.shape)
 print(Y_train.shape, Y_val.shape, Y_test.shape)
 
+# print(np.isnan(X_train).any())  # Returns True if any NaN exists
+# print(np.isnan(Y_train).any())  # Returns True if any NaN exists
+
+# Initialize and setup model
 model = Sequential([
     LSTM(256, return_sequences=True, input_shape=(X_train.shape[1],X_train.shape[2])),
     LSTM(256, return_sequences=True),
@@ -114,18 +109,15 @@ model.compile(loss='mse',
                 metrics=['mae', 'mse'])
 model.summary()
 
-EPOCHS = 3
+# Configure and run model training
+EPOCHS = 30
 
-print(type(X_val))  # Should be <class 'numpy.ndarray'>
-print(type(Y_val))  
-
-# print(np.isnan(X_train).any())  # Check for NaNs
-# print(np.isfinite(X_train).all()) 
 print(f"X_train dtype: {X_train.dtype}")
 print(f"Y_train dtype: {Y_train.dtype}")
 
 history = model.fit(x = X_train, y = Y_train, epochs=EPOCHS, validation_data = (X_val, Y_val), shuffle=False, verbose=1)
 
+# Plot training losses
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('model loss')
@@ -134,6 +126,7 @@ plt.xlabel('epoch')
 plt.legend(['train', 'validation'], loc='upper left')
 plt.show()
 
+# Run Model Testing
 test_predictions = model.predict(X_test)
 op_list= [e for y in test_predictions for e in y]   #predicted list
 ip_list = [e for y in Y_test for e in y]    #actual list
@@ -171,19 +164,19 @@ for threshold in [0.5,1,1.5,2,3,4,5]:
     accuracy = sum(abs(e) < threshold for e in rerror)/len(error)*100
     print('accuracy:', accuracy,'% with threshold of', threshold)
 
-import matplotlib
-matplotlib.rcParams.update({'font.size': 16})
-
+# Plot Absolute Error
 plt.figure(figsize=(5,5))
 plt.hist(error, bins = 50)
 plt.xlabel("Prediction Absolute Error")
 _ = plt.ylabel("Count")
 
+# Plot Prediction Error
 plt.figure(figsize=(5,5))
 plt.hist(rerror, bins = 50)
 plt.xlabel("Prediction Error (%)")
 _ = plt.ylabel("Count")
 
+# Plot True Value vs Predictions
 plt.figure(figsize=(10,5))
 a = plt.axes(aspect='equal')
 plt.scatter(test_labels['actual'], predictions)
@@ -195,41 +188,17 @@ plt.ylim(lims)
 _ = plt.plot(lims, lims)
 
 end = datetime.datetime.now()
-print('total time:', end-start)
+# print('total time:', end-start)
 
-from keras.models import save_model
+
 save_model(model,'Models/LSTM_model_3.0.1.h5',include_optimizer=True)
 np.save('Models/LSTM_history_3.0.1.npy',history.history)
 
-import numpy as np
 history = np.load('Models/LSTM_history_3.0.1.npy', allow_pickle=True)[()]
-print(history)
+# print(history)
 
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.rcParams.update({'font.size': 16})
 
-plt.figure(figsize=(5,5))
-plt.plot(history['loss'])
-plt.plot(history['val_loss'])
-# plt.title('model loss')
-plt.ylabel('model loss')
-plt.xlabel('epoch number')
-plt.legend(['train', 'validation'], loc='upper left')
-plt.show()
-
-from keras.models import load_model
 custom_objects = {'mse' : 'mean_squared_error'}
 model = load_model('Models/LSTM_model_3.0.1.h5', custom_objects=custom_objects)
 
 np.save('Models/predictions1.0', np.array(predictions['predict']))
-
-# for i in range(0, len(normed_data)):
-#     print(normed_data(i))
-# print(normed_data)
-
-# # Write CSV file
-# with open("test.csv", "wt") as fp:
-#     writer = csv.writer(fp, delimiter=",")
-#     # writer.writerow(["your", "header", "foo"])  # write header
-#     writer.writerows(dataset)
